46217a30a304c5022737c60387f360afc7db6361
Repap <saml.repap@gmail.com>
2020-03-09 19:26:56 -0400

    Added font and profile image, fixed a few css issues

--- react-frontend/src/pages/home/Home.js
+++ react-frontend/src/pages/home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import profile from './../../res/samuel.png';
+
 class Home extends React.Component {
 
   render() {
     return (
       <div className="page">
+        <img className="profile" src={profile} alt="Profile"/>
         <h1>Hi, I'm Samuel.</h1>
       </div>
     );
