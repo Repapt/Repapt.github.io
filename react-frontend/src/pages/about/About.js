7a4628e4949129a7f094f203f4eaf07efbf6bb6b
Repap <saml.repap@gmail.com>
2020-03-12 00:01:20 -0400

    added resume and github buttons

--- react-frontend/src/pages/about/About.js
+++ react-frontend/src/pages/about/About.js
@@ -5,9 +5,11 @@ class About extends React.Component {
   render() {
     return (
       <div className="page">
-        <h1>About me.</h1>
-        <h3>Computer Engineer at the University of Waterloo.</h3>
-        <h3>Also I like Marvel.</h3>
+        <div className="text">
+          <h1>About me.</h1>
+          <h3>Computer Engineer at the University of Waterloo.</h3>
+          <h3>Also I like Marvel.</h3>
+        </div>  
       </div>
     );
   }
--- react-frontend/src/pages/home/Home.js
+++ react-frontend/src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import profile from './../../res/samuel.png';
+import resume from './../../res/resume.pdf';
 
 class Home extends React.Component {
 
@@ -8,9 +9,13 @@ class Home extends React.Component {
     return (
       <div className="page">
         <img className="profile" src={profile} alt="Profile"/>
-        <h1>Hi, I'm Samuel.</h1>
-        <h2>I'm a Computer Engineering student</h2>
-        <h2>at the University of Waterloo.</h2>
+        <div className="text">
+          <h1>Hi, I'm Samuel.</h1>
+          <h2>I'm a Computer Engineering student</h2>
+          <h2>at the University of Waterloo.</h2>
+        </div>
+        <a className="button" href={ resume } target="_blank">Resume</a>
+        <a className="button" href="https://www.github.com/Repapt" target="_blank">Github</a>
       </div>
     );
   }
