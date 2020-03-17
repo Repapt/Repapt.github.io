1ccb530da68c7593a9fef21ed66570491d9aa458
Repap <saml.repap@gmail.com>
2020-03-17 19:34:07 -0400

    fixed re-sizing issues

--- react-frontend/src/pages/contact/Contact.js
+++ react-frontend/src/pages/contact/Contact.js
@@ -5,7 +5,9 @@ class Contact extends React.Component {
   render() {
     return (
       <div className="page">
-        <h1>How to reach me.</h1>
+        <div className="text">
+          <h1>How to reach me.</h1>
+        </div>
       </div>
     );
   }
