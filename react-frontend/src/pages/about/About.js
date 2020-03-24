b40bace94c1f0f7256c86af0e992b1676c54c6a5
Repap <saml.repap@gmail.com>
2020-03-23 22:44:50 -0400

    Tweaked about page

--- react-frontend/src/pages/about/About.js
+++ react-frontend/src/pages/about/About.js
@@ -7,11 +7,11 @@ class About extends React.Component {
   render() {
     return (
       <div className="page">
-        <div className="text">
+        <div id="about-text" className="text">
           <h2 className="title">I've always been driven by my passions.</h2>
           <p className="body">Growing up, I loved solving puzzles. I've played with everything from rubik's cubes to</p>
-          <p className="body">escape rooms and video games. Inevitably, though, my passions eventually shifted to a</p>
-          <p className="body">slightly different form of problem-solving: programming. I've worked with web design, backend</p>
+          <p className="body">escape rooms and video games. Eventually, though, my passions shifted to a slightly</p>
+          <p className="body">different form of problem-solving: programming. I've worked with web design, backend</p>
           <p className="body">development, and machine learning. In my free time, I enjoy swimming, basketball, cooking,</p>
           <p className="body">and guitar. I'm always listening to music - it's usually rock, but I can listen to anything.</p>
           <p className="body">I'm an easy cry at the movies (especially if it's animated!) and I'm a huge Marvel nerd.</p>
