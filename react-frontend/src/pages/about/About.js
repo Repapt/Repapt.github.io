00ecca022b8cd8b8e03707bf2e4c2382d9aefced
Repap <saml.repap@gmail.com>
2020-04-27 04:27:27 -0400

    implemented email sending, modified about page

--- react-frontend/src/pages/about/About.js
+++ react-frontend/src/pages/about/About.js
@@ -8,15 +8,19 @@ class About extends React.Component {
     return (
       <div className="page">
         <div className="text">
-          <h2>I've always been driven by my passions.</h2>
-          <p className="body">As a kid, I loved solving puzzles. Rubik's cubes and crosswords were among my favourites. Going</p>
-          <p className="body">into high school, I still loved puzzles but the problems I was solving were no longer found on the</p>
-          <p className="body">backs of newspapers, and the solutions were written in lines of Java. Today, I love web design, making</p>
-          <p className="body">games, backend development, and machine learning. My free time is spent on TV shows, swimming,</p>
-          <p className="body">cooking, and playing guitar. I'm always listening to music, though admittedly a bit too loudly at times.</p>
-          <p className="body">I'm an easy cry at the movies (especially if it's animated!) and I'm a huge Marvel nerd.</p>
+          <h2>I've always been a passionate person.</h2>
+          <p className="body">
+            As a kid, I was obsessed with puzzles. It started off with the rubik's cube, but as I grew older,
+            I started gravitating towards a slightly different kind of puzzle; the kind that was written in lines of python.
+            I've wanted to be a programmer ever since. Today, I love web
+            design, making games, backend development, and machine learning. My free time is spent on TV shows,
+            swimming, cooking, and playing guitar. I'm an easy cry at the movies (especially if it's animated!)
+            and I'm a huge Marvel nerd.
+          </p>
           <p className="body">&nbsp;</p>
-          <p className="body">These passions have characterized my life. Now, I'm looking forward to my next one.</p>
+          <p className="body">
+            These passions have characterized my life. Right now, I'm looking forward to my next one.
+          </p>
           <img className="profile" src={about_profile} alt="Profile"></img>
         </div>
       </div>
--- react-frontend/src/pages/contact/Contact.js
+++ react-frontend/src/pages/contact/Contact.js
@@ -2,11 +2,89 @@ import React from 'react';
 
 class Contact extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    };
+    this.handleEntry = this.handleEntry.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleEntry(event) {
+    const field = event.target.id;
+    const value = event.target.value;
+    this.setState({
+      ...this.state,
+      [field]: value,
+    })
+  }
+
+  handleSubmit() {
+    const template_id = "personal_website";
+    const variables = this.state;
+    window.emailjs.send(
+      'gmail',
+      template_id,
+      variables
+    ).then(res => {
+      if(res.ok){
+        alert("Message successfully sent!")
+      } else {
+        throw Error("Message failed to send.")
+      }
+    }).catch(alert)
+  }
+
   render() {
     return (
       <div className="page">
         <div className="text">
-          <h2>How to reach me.</h2>
+          <h2 className="contact-title">How to reach me</h2>
+          <div className="contact-options">
+            <div className="email">
+              <h4>Feel free to shoot me a message:</h4>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Name"
+                value={this.state.name}
+                onChange={this.handleEntry}
+              />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                value={this.state.email}
+                onChange={this.handleEntry}
+              />
+              <input
+                type="text"
+                id="subject"
+                name="subject"
+                placeholder="Subject"
+                value={this.state.subject}
+                onChange={this.handleEntry}
+              />
+              <textarea
+                type="text"
+                id="message"
+                name="message"
+                placeholder="Message"
+                value={this.state.message}
+                onChange={this.handleEntry}
+              />
+              <div className="contact-button" onClick={this.handleSubmit}>Send</div>
+            </div>
+            <div className="socials">
+              <h4>Or DM me on any of these social media platforms:</h4>
+            </div>  
+          </div>      
         </div>
       </div>
     );
