07353f79234b22ff3feb70ba98bdf3684baf2abb
Repap <saml.repap@gmail.com>
2020-05-26 21:35:18 -0400

    updated resume

--- react-frontend/src/pages/about/About.js
+++ react-frontend/src/pages/about/About.js
@@ -8,9 +8,10 @@ class About extends React.Component {
     return (
       <div className="page">
         <div className="text">
-          <h2>I've always been a passionate person.</h2>
+          <h2>I was always a passionate person.</h2>
           <p className="body">
-            As a kid, I loved puzzles. The Rubik's Cube was a personal favourite.
+            As a kid, my passion was for puzzles. I've spent countless hours of my childhood fiddling with
+            Rubik's Cubes, board games - anything I could get my hands on.
             Going into high school, my puzzle-loving
             brain pushed me to take a computer science course, where I became acquainted with the world of competitive programming.
             I've wanted to be a programmer ever since. Today, I love web
@@ -20,7 +21,7 @@ class About extends React.Component {
           </p>
           <p className="body">&nbsp;</p>
           <p className="body">
-            These passions have characterized my life. Right now, I'm looking forward to my next one.
+            These passions have characterized my life. Now, I'm looking forward to my next one.
           </p>
           <img className="profile" src={about_profile} alt="Profile"></img>
         </div>
