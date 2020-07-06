a94230c611bbc613dab370745303b5f9bbbef944
Repap <saml.repap@gmail.com>
2020-07-05 20:21:47 -0400

    update about page

--- react-frontend/src/pages/about/About.js
+++ react-frontend/src/pages/about/About.js
@@ -8,20 +8,13 @@ class About extends React.Component {
     return (
       <div className="page">
         <div className="text">
-          <h2>I was always a passionate person.</h2>
+          <h2>About me.</h2>
           <p className="body">
-            As a kid, my passion was for puzzles. I've spent countless hours of my childhood fiddling with
-            Rubik's Cubes, board games - anything I could get my hands on.
-            Going into high school, my puzzle-loving
-            brain pushed me to take a computer science course, where I became acquainted with the world of competitive programming.
-            I've wanted to be a programmer ever since. Today, I love web
-            design, making games, backend development, and machine learning. My free time is spent on TV shows,
-            swimming, cooking, and playing guitar. I'm an easy cry at the movies (especially if it's animated!)
-            and I'm a huge Marvel nerd.
-          </p>
-          <p className="body">&nbsp;</p>
-          <p className="body">
-            These passions have characterized my life. Now, I'm looking forward to my next one.
+            A reliable and independent critical thinker with an attention to detail, I first started programming by competing in various coding competitions.
+            Since then, I've branched out into web design, creating games, and whatever else piques my interest.
+            I spend my free time on TV shows,
+            video games, swimming, cooking, and playing guitar. I'm a sucker for animated movies,
+            and I'm kind of a Marvel nerd.
           </p>
           <img className="profile" src={about_profile} alt="Profile"></img>
         </div>
