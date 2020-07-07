1f040e64151fc6b651d1c872b5cd219a2f2ca3f4
Repap <saml.repap@gmail.com>
2020-07-07 03:19:28 -0400

    updated about me

--- react-frontend/src/pages/about/About.js
+++ react-frontend/src/pages/about/About.js
@@ -8,10 +8,12 @@ class About extends React.Component {
     return (
       <div className="page">
         <div className="text">
-          <h2>About me.</h2>
+          <h2>About me</h2>
           <p className="body">
             A reliable and independent critical thinker with an attention to detail, I first started programming by competing in various coding competitions.
             Since then, I've branched out into web design, creating games, and whatever else piques my interest.
+          </p>
+          <p className="body">
             I spend my free time on TV shows,
             video games, swimming, cooking, and playing guitar. I'm a sucker for animated movies,
             and I'm kind of a Marvel nerd.
